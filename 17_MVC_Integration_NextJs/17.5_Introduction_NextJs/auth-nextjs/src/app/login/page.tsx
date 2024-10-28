374dee8787e7d87d3c418a33ed08cc5cce5bb24f
sandhi-sayem <88631568+sandhi-sayem@users.noreply.github.com>
2024-10-27 21:56:24 -0400

    Starting 17.5 nextjs tutorial

---
+++ 17_MVC_Integration_NextJs/17.5_Introduction_NextJs/auth-nextjs/src/app/login/page.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+  return (
+    <div className="flex">
+      <h1 className="text-center text-white text-2xl">Login</h1>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ 17_MVC_Integration_NextJs/17.5_Introduction_NextJs/auth-nextjs/src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+const SignupPage = () => {
+  return (
+    <div className="flex">
+      <h1 className="text-center text-white text-2xl">Signup</h1>
+    </div>
+  );
+};
+
+export default SignupPage;
