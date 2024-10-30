889c86d7127a5c89ae230bbfa08a03da76964664
sandhi-sayem <88631568+sandhi-sayem@users.noreply.github.com>
2024-10-29 20:55:22 -0400

    Finish 17.6 section 3 login and signup nextjs

--- 17_MVC_Integration_NextJs/17.6_NextJs_Integrating_F_B/auth-nextjs/src/app/login/page.tsx
+++ 17_MVC_Integration_NextJs/17.6_NextJs_Integrating_F_B/auth-nextjs/src/app/login/page.tsx
@@ -1,21 +1,43 @@
 "use client";
 
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { axios } from "axios";
+import axios from "axios";
 
 const LoginPage = () => {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   });
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
 
-  const onLogin = async () => {};
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login success", response.data);
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("Login failed", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Login</h1>
+      <h1>{loading ? "Processing" : "Login"}</h1>
       <hr />
       <label htmlFor="email">email</label>
       <input
@@ -39,7 +61,7 @@ const LoginPage = () => {
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
         onClick={onLogin}
       >
-        Login here
+        {buttonDisabled ? "No login" : "Login"}
       </button>
       <Link href="/signup">Visit Signup page</Link>
     </div>
