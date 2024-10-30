36a032f1a5bda435a57e72072bb700b3047275d7
sandhi-sayem <88631568+sandhi-sayem@users.noreply.github.com>
2024-10-29 21:57:08 -0400

    Finish 17.6 middleware nextjs

--- 17_MVC_Integration_NextJs/17.6_NextJs_Integrating_F_B/auth-nextjs/src/app/profile/page.tsx
+++ 17_MVC_Integration_NextJs/17.6_NextJs_Integrating_F_B/auth-nextjs/src/app/profile/page.tsx
@@ -1,9 +1,54 @@
+"use client";
+
+import axios from "axios";
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 const ProfilePage = () => {
+  const router = useRouter();
+  const [data, setData] = React.useState("nothing");
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      console.log("Logout successful");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
       <p>Profile page</p>
+      <h2 className="p-1 rounded bg-green-500">
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
+      <hr />
+      <button
+        onClick={logout}
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Logout
+      </button>
+      <button
+        onClick={getUserDetails}
+        className="bg-green-500 mt-4 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Get User Details
+      </button>
     </div>
   );
 };
